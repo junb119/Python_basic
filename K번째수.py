4b8a66b130f358b53b06b161eeec07d6cc829046
jun <emfowkd@gmail.com>
2022-04-29 14:25:42 +0900

    add 폰켓몬

--- K번째수.py
+++ K번째수.py
@@ -36,6 +36,8 @@ def solution(array, commands):
 ver2
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+    
+    
 # "for command in commands: i,j,k = command" 대신 "for i,j,k in commands:" 로 한 줄로 줄여 쓰면 더 편해요!
 
 
